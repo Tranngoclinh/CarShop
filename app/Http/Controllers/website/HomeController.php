bc4a2f20c34d2b69914c6ed7dfb8d520a7a8878d
Tranngoclinh <nguyenlinh23444@gmail.com>
2022-08-02 13:54:29 +0700

    CarShop

---
+++ app/Http/Controllers/website/HomeController.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Http\Controllers\website;
+
+use App\Http\Controllers\Controller;
+use App\Models\admin\CustomerModel;
+use App\Models\admin\ProductModel;
+use App\Models\Cart;
+// use Illuminate\Contracts\Session\Session;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Session as FacadesSession;
+
+class HomeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = ProductModel::all();
+        // dd($products);
+        return view('website.index', compact('products'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function detail($id)
+    {
+        $product = ProductModel::findOrFail($id);
+        // dd($products);
+        return view('website.detail', compact('product'));
+    }
+    public function cart()
+    {
+        return view('website.cart');
+    }
+    public function addToCart(Request $request, $id)
+    {
+        $product = ProductModel::findOrFail($id);
+        $cart = session()->get('cart', []);
+        if (isset($cart[$id])) {
+            $cart[$id]['quantity']++;
+            // dd($cart[$id]);
+        } else {
+            $cart[$id] = [
+                "name" => $product->name,
+                "quantity" =>1,
+                "price" => $product->price,
+                "image" => $product->image,
+                "product" => $product->id, 
+            ];
+        }
+        // dd($cart);
+
+        // $total = $product->price*$product->quantity;
+        session()->put('cart', $cart);
+        return redirect()->back()->with('success', 'Product added to cart successfully!');
+        return back();
+    }
+
+    public function updatecart(Request $request)
+    {
+        if ($request->items['product'] && $request->items['quantity']) {
+            $cart = session()->get('cart');
+            $cart[$request->items['product']]->items["quantity"] = $request->quantity;
+            session()->put('cart', $cart);
+            session()->flash('success', 'Cart updated successfully');
+        }
+        return redirect()->route('home.cart');
+    }
+    
+    public function getCart(Request $request)
+    {
+        if ($request->id) {
+            $cart = session()->get('cart');
+            if (isset($cart[$request->id])) {
+                unset($cart[$request->id]);
+                session()->put('cart', $cart);
+            }
+            session()->flash('success', 'Product removed successfully');
+        }
+    }
+    public function remove(Request $request,$id)
+    {
+        if ($request->id) {
+            $cart = session()->get('cart');
+            // dd($cart);
+            if (isset($cart[$request->id])) {
+                unset($cart[$request->id]);
+                session()->put('cart', $cart);
+            }
+            // session()->flash('success', 'Product removed successfully');
+            return view('website.cart');
+        }
+    }
+
+    public function destroy($id)
+    {
+        //
+    }
+
+    public function ordered(Request $request){
+        $customer = new CustomerModel();
+        $customer->name = $request->name;
+        $customer->email = $request->email;
+        $customer->address = $request->address;
+        $customer->phone = $request->phone;
+        $customer->save();
+        Session::flash('success', 'Đã Đặt Hàng');
+        return redirect()->route('home.cart');
+    }
+
+    public function cars(){
+        $products = ProductModel::all();
+        return view('website.car',compact('products'));
+    }
+
+}
